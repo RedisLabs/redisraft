5c81b099d0c330d326d716751dfaa6f5cbfd6ce9
Yossi Gottlieb <yossigo@gmail.com>
2018-02-28 08:46:55 +0200

    Don't run valgrind by default.

--- run.sh
+++ run.sh
@@ -16,11 +16,11 @@ colorize() {
     done
 }
 
-#../redis/src/redis-server --port 5001 --loadmodule `pwd`/redisraft.so id=1 node=2,localhost:5002 node=3,localhost:5003 2>&1 | colorize yellow&
-#../redis/src/redis-server --port 5002 --loadmodule `pwd`/redisraft.so id=2 node=1,localhost:5001 node=3,localhost:5003 2>&1 | colorize magenta&
-#../redis/src/redis-server --port 5003 --loadmodule `pwd`/redisraft.so id=3 node=1,localhost:5001 node=2,localhost:5002 2>&1 | colorize cyan&
+../redis/src/redis-server --port 5001 --loadmodule `pwd`/redisraft.so id=1 node=2,localhost:5002 node=3,localhost:5003 2>&1 | colorize yellow&
+../redis/src/redis-server --port 5002 --loadmodule `pwd`/redisraft.so id=2 node=1,localhost:5001 node=3,localhost:5003 2>&1 | colorize magenta&
+../redis/src/redis-server --port 5003 --loadmodule `pwd`/redisraft.so id=3 node=1,localhost:5001 node=2,localhost:5002 2>&1 | colorize cyan&
 
-valgrind --leak-check=full --show-reachable=yes --log-file=vg1.log ../redis/src/redis-server --port 5001 --loadmodule `pwd`/redisraft.so id=1 node=2,localhost:5002 node=3,localhost:5003 2>&1 &
-valgrind --leak-check=full --show-reachable=yes --log-file=vg2.log ../redis/src/redis-server --port 5002 --loadmodule `pwd`/redisraft.so id=2 node=1,localhost:5001 node=3,localhost:5003 2>&1 &
-valgrind --leak-check=full --show-reachable=yes --log-file=vg2.log ../redis/src/redis-server --port 5003 --loadmodule `pwd`/redisraft.so id=3 node=1,localhost:5001 node=2,localhost:5002 2>&1 &
+#valgrind --leak-check=full --show-reachable=yes --log-file=vg1.log ../redis/src/redis-server --port 5001 --loadmodule `pwd`/redisraft.so id=1 node=2,localhost:5002 node=3,localhost:5003 2>&1 &
+#valgrind --leak-check=full --show-reachable=yes --log-file=vg2.log ../redis/src/redis-server --port 5002 --loadmodule `pwd`/redisraft.so id=2 node=1,localhost:5001 node=3,localhost:5003 2>&1 &
+#valgrind --leak-check=full --show-reachable=yes --log-file=vg2.log ../redis/src/redis-server --port 5003 --loadmodule `pwd`/redisraft.so id=3 node=1,localhost:5001 node=2,localhost:5002 2>&1 &
 read
