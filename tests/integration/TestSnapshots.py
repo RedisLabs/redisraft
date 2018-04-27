06171d6495de3e65c4ac989739f7cb4ced73e0a6
Yossi Gottlieb <yossigo@gmail.com>
2018-04-27 23:03:11 +0300

    Improve join after snapshot test.

--- tests/integration/TestSnapshots.py
+++ tests/integration/TestSnapshots.py
@@ -31,12 +31,18 @@ def test_cfg_from_snapshot(c):
     """
     c.create(2, raft_args={'persist': 'no'})
     for i in range(100):
-        eq_(c.node(1).raft_exec('SET', 'key%s' % i, 'val%s' % i), b'OK')
+        c.node(1).raft_exec('SET', 'key%s' % i, 'val%s' % i)
+        c.node(1).raft_exec('INCR', 'counter')
 
     # Make sure log is compacted
     eq_(c.node(1).client.execute_command('RAFT.DEBUG', 'COMPACT'), b'OK')
     eq_(0, c.node(1).raft_info()['log_entries'])
 
-    # Add new node
+    # Add new node and wait for things to settle
     r3 = c.add_node()
     r3.wait_for_election()
+
+    # Make sure we have what we expect
+    for i in range(100):
+        eq_(str(c.node(3).client.get('key%s' % i), 'utf-8'), 'val%s' % i)
+    eq_(c.node(3).client.get('counter'), b'100')
