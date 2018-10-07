19253cee195f565f03169dc8581b485c8aa0973c
Yossi Gottlieb <yossigo@gmail.com>
2018-10-07 15:22:13 +0300

    Fix test false positives.

--- tests/integration/TestSnapshots.py
+++ tests/integration/TestSnapshots.py
@@ -123,7 +123,7 @@ def test_cfg_node_removed_from_snapshot(c):
     # bring back node 4
     c.node(4).start()
     c.node(4).wait_for_election()
-    c.node(4).wait_for_log_applied()
+    c.wait_for_unanimity()
     eq_(4, c.node(4).raft_info()['num_nodes'])
 
 @with_setup_args(_setup, _teardown)
