831a2b37e0d89b379e3f3567fd8e31872d834105
Yossi Gottlieb <yossigo@gmail.com>
2018-10-29 18:27:40 +0200

    Docker build update.

--- jepsen/docker/build/build.sh
+++ jepsen/docker/build/build.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -e
 
+REDIS_VERSION=${REDIS_VERSION:-5.0.0}
 if [ -z "$VERSION" ]; then
     echo "Missing VERSION environment variable!"
     exit 1
 fi
 
-git clone --recursive https://github.com/yossigo/redisraft
-git clone https://github.com/antirez/redis
+if [ ! -f /src/redisraft.h ]; then
+    echo "Redis Raft module source not found!"
+    exit 1
+fi
+
+mkdir -p /work/redisraft
+cd /work/redisraft
+(cd /src ; tar cf - * --exclude jepsen) | tar xf -
+
+cd /work
+curl -L https://github.com/antirez/redis/archive/${REDIS_VERSION}.tar.gz | tar xfz -
+mv redis-${REDIS_VERSION} redis
 
 make -C redis
 make -C redisraft
