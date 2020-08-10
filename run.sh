dc172b4a2ec454e7a973d265320b37ed150d31ea
Yossi Gottlieb <yossigo@gmail.com>
2020-08-10 17:49:47 +0300

    Fix run.sh to properly run redis-server.

--- run.sh
+++ run.sh
@@ -5,20 +5,20 @@ LOGLEVEL=notice
     --loglevel $LOGLEVEL \
     --logfile redis1.log \
     --dbfilename redis1.rdb \
-    --loadmodule `pwd`/redisraft.so raft-log-filename=raftlog1.db addr=localhost:5001
+    --loadmodule `pwd`/redisraft.so raft-log-filename=raftlog1.db addr=localhost:5001 &
 
 ../redis/src/redis-server \
     --port 5002 \
     --loglevel $LOGLEVEL \
     --logfile redis2.log \
     --dbfilename redis2.rdb \
-    --loadmodule `pwd`/redisraft.so raft-log-filename=raftlog2.db addr=localhost:5002
+    --loadmodule `pwd`/redisraft.so raft-log-filename=raftlog2.db addr=localhost:5002 &
 
 ../redis/src/redis-server \
     --port 5003 \
     --loglevel $LOGLEVEL \
     --logfile redis3.log \
-    --loadmodule `pwd`/redisraft.so raft-log-filename=raftlog3.db addr=localhost:5003
+    --loadmodule `pwd`/redisraft.so raft-log-filename=raftlog3.db addr=localhost:5003 &
 
 sleep 1
 redis-cli -p 5001 raft.cluster init
