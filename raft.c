d7b932c99447d8ca81c3ecd234b837acee8dcc36
Ozan Tezcan <ozantezcan@gmail.com>
2021-08-31 16:32:58 +0300

    Fix unsynchronized access on configuration change (#137)
    Fix unsynchronized access while adding/removing a node

--- raft.c
+++ raft.c
@@ -1320,7 +1320,6 @@ static void handleCfgChange(RedisRaftCtx *rr, RaftReq *req)
 {
     raft_entry_t *entry;
     msg_entry_response_t response;
-    raft_node_t *rn;
     int e;
 
     if (checkRaftState(rr, req) == RR_ERROR ||
@@ -1328,26 +1327,39 @@ static void handleCfgChange(RedisRaftCtx *rr, RaftReq *req)
         goto exit;
     }
 
-    entry = raft_entry_new(sizeof(req->r.cfgchange));
-    entry->id = rand();
+    short type;
 
     switch (req->type) {
         case RR_CFGCHANGE_ADDNODE:
-            entry->type = RAFT_LOGTYPE_ADD_NONVOTING_NODE;
+            if (hasNodeIdBeenUsed(rr, req->r.cfgchange.id)) {
+                RedisModule_ReplyWithError(req->ctx,
+                               "node id has already been used in this cluster");
+                goto exit;
+            }
+
+            type = RAFT_LOGTYPE_ADD_NONVOTING_NODE;
             if (!req->r.cfgchange.id) {
                 req->r.cfgchange.id = makeRandomNodeId(rr);
             }
             break;
         case RR_CFGCHANGE_REMOVENODE:
-            rn = raft_get_node(rr->raft, req->r.cfgchange.id);
-            assert (rn != NULL);    /* Should have been verified by now! */
-            entry->type = RAFT_LOGTYPE_REMOVE_NODE;
+            /* Validate it exists */
+            if (!raft_get_node(rr->raft, req->r.cfgchange.id)) {
+                RedisModule_ReplyWithError(req->ctx, "node id does not exist");
+                goto exit;
+            }
+
+            type = RAFT_LOGTYPE_REMOVE_NODE;
             break;
         default:
             assert(0);
     }
 
+    entry = raft_entry_new(sizeof(req->r.cfgchange));
+    entry->id = rand();
+    entry->type = type;
     memcpy(entry->data, &req->r.cfgchange, sizeof(req->r.cfgchange));
+
     if ((e = raft_recv_entry(rr->raft, entry, &response)) != 0) {
         replyRaftError(req->ctx, e);
     } else {
