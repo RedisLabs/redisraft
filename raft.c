285fceddb4c8df1a741a09b61b632a87f7f5f116
tezc <ozantezcan@gmail.com>
2022-01-08 06:55:08 +0300

    Add leak

--- raft.c
+++ raft.c
@@ -1347,7 +1347,11 @@ void RaftReqFree(RaftReq *req)
             }
             break;
         case RR_SHARDGROUP_ADD:
-            ShardGroupTerm(&req->r.shardgroup_add);
+            //ShardGroupTerm(&req->r.shardgroup_add);
+            if (req->r.shardgroup_add.nodes) {
+                RedisModule_Free(req->r.shardgroup_add.nodes);
+                req->r.shardgroup_add.nodes = NULL;
+            }
             break;
         case RR_SHARDGROUPS_REPLACE:
             if (req->r.shardgroups_replace.shardgroups != NULL) {
