53dd50b98448d976493f58612162131150d4543a
Shaya Potter <shaya@redislabs.com>
2022-11-13 12:55:35 +0200

    includes requested ci changes

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -50,9 +50,9 @@ def pytest_addoption(parser):
         '--elle-cli', default='../elle-cli/target/elle-cli-0.1.4-standalone.jar',
         help='location for elle-cli jar file')
     parser.addoption(
-        '--elle_threads', default=0, help='number of elle worker threads')
+        '--elle-threads', default=0, help='number of elle worker threads')
     parser.addoption(
-        '--elle_ops_per_tx', default=1, help='number of append/read pairs per transaction')
+        '--elle-ops-per-tx', default=1, help='number of append/read pairs per transaction')
 
 
 def pytest_configure(config):
@@ -83,8 +83,8 @@ def create_config(pytest_config):
     config.fsync = pytest_config.getoption('--fsync')
     config.tls = pytest_config.getoption('--tls')
     config.elle_cli = os.path.abspath(pytest_config.getoption('--elle-cli'))
-    config.elle_threads = int(pytest_config.getoption('--elle_threads'))
-    config.elle_num_ops = int(pytest_config.getoption('--elle_ops_per_tx'))
+    config.elle_threads = int(pytest_config.getoption('--elle-threads'))
+    config.elle_num_ops = int(pytest_config.getoption('--elle-ops-per-tx'))
 
     if pytest_config.getoption('--valgrind'):
         if config.args is None:
