4c7d09885e7ec4f346778ef1ed82affad4efa716
Ozan Tezcan <ozantezcan@gmail.com>
2022-12-07 17:41:51 +0300

    Add CRC32c implementation

---
+++ deps/common/sc_crc32.h
@@ -0,0 +1,53 @@
+/*
+* BSD-3-Clause
+*
+* Copyright 2021 Ozan Tezcan
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its contributors
+*    may be used to endorse or promote products derived from this software
+*    without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+* OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+* OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef SC_CRC32_H
+#define SC_CRC32_H
+
+#include <stdint.h>
+
+#define SC_CRC32_VERSION "2.0.0"
+
+/**
+* Call once globally.
+*/
+void sc_crc32_init(void);
+
+/**
+* @param crc initial value, if you're not calculating crc from partial buffers,
+*            it should be zero.
+* @param buf buf
+* @param len len
+* @return    crc value
+*/
+uint32_t sc_crc32(uint32_t crc, const uint8_t *buf, uint32_t len);
+
+#endif
