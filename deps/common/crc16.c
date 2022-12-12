91f0ad7b40d240edfc41f41d96cfde1d7683a4f5
Shaya Potter <shaya@redislabs.com>
2022-12-12 10:08:00 +0200

    revert crc16 change as no longer needed as moved to crc32 code

--- deps/common/crc16.c
+++ deps/common/crc16.c
@@ -63,11 +63,11 @@ static const unsigned short crc16tab[256]= {
 	0xef1f,0xff3e,0xcf5d,0xdf7c,0xaf9b,0xbfba,0x8fd9,0x9ff8,
 	0x6e17,0x7e36,0x4e55,0x5e74,0x2e93,0x3eb2,0x0ed1,0x1ef0
 };
-
-unsigned short crc16_ccitt(unsigned short crc_start, const char *buf, int len)
+  
+unsigned short crc16_ccitt(const char *buf, int len)
 {
 	register int counter;
-	register unsigned short crc = crc_start; 
+	register unsigned short crc = 0;
 	for( counter = 0; counter < len; counter++)
 		crc = (crc<<8) ^ crc16tab[((crc>>8) ^ *buf++)&0x00FF];
 	return crc;
