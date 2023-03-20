a0054ea73d7341d12f00731cb66cdb3ef41a47f1
LLiuJJ <jay_jieliu@outlook.com>
2023-03-21 00:46:18 +0800

    fix clang-format style check

--- src/redisraft.h
+++ src/redisraft.h
@@ -414,18 +414,18 @@ typedef struct RedisRaftCtx {
     struct CommandSpecTable *commands_spec_table;
 
     /* General stats */
-    unsigned long client_attached_entries;          /* Number of log entries attached to user connections */
-    unsigned long long proxy_reqs;                  /* Number of proxied requests */
-    unsigned long long proxy_failed_reqs;           /* Number of failed proxy requests, i.e. did not send */
-    unsigned long long proxy_failed_responses;      /* Number of failed proxy responses, i.e. did not complete */
-    unsigned long proxy_outstanding_reqs;           /* Number of proxied requests pending */
-    unsigned long snapshots_received;               /* Number of received snapshots */
-    unsigned long snapshots_created;                /* Number of snapshots created */
-    unsigned long appendreq_received;               /* Number of received appendreq messages */
-    unsigned long appendreq_with_entry_received;    /* Number of received appendreq messages with at least one entry in them */
-    unsigned long snapshotreq_received;             /* Number of received snapshotreq messages */
-    unsigned long exec_throttled;                   /* Number of command executions throttled due to slow execution */
-    unsigned long importkeys_restore_failed_count;  /* Number of restore command failed count during raft import keys */
+    unsigned long client_attached_entries;         /* Number of log entries attached to user connections */
+    unsigned long long proxy_reqs;                 /* Number of proxied requests */
+    unsigned long long proxy_failed_reqs;          /* Number of failed proxy requests, i.e. did not send */
+    unsigned long long proxy_failed_responses;     /* Number of failed proxy responses, i.e. did not complete */
+    unsigned long proxy_outstanding_reqs;          /* Number of proxied requests pending */
+    unsigned long snapshots_received;              /* Number of received snapshots */
+    unsigned long snapshots_created;               /* Number of snapshots created */
+    unsigned long appendreq_received;              /* Number of received appendreq messages */
+    unsigned long appendreq_with_entry_received;   /* Number of received appendreq messages with at least one entry in them */
+    unsigned long snapshotreq_received;            /* Number of received snapshotreq messages */
+    unsigned long exec_throttled;                  /* Number of command executions throttled due to slow execution */
+    unsigned long importkeys_restore_failed_count; /* Number of restore command failed count during raft import keys */
 
     int entered_eval;                     /* handling a lua script */
     RedisModuleDict *locked_keys;         /* keys that have been locked for migration */
