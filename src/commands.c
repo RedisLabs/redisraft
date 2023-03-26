35e0c84a4bc511f95531c028f98301f2ad00c3e5
Shaya Potter <shaya@redislabs.com>
2023-03-26 12:53:50 +0300

    add comments

--- src/commands.c
+++ src/commands.c
@@ -389,20 +389,20 @@ int CommandSpecTableGetFlags(CommandSpecTable *cmd_spec_table, RedisModuleDict *
         return flags;
     }
 
-    cmd_spec_table = RedisModule_DictGetC(sub_command_tables, cs->name, strlen(cs->name), NULL);
-    if (cmd_spec_table == NULL) {
-        return flags;
-    }
-
     /* flags are now for subcommand, so mask it out of flags */
-    flags &= ~CMD_SPEC_SUBCOMMAND;
+    flags &= ~CMD_SPEC_SUBCOMMAND;  /* flags is now the default set of flags for all subcommands */
 
-    CommandSpec *sub_cs = getOrCreateCommandSpec(cmd_spec_table, subcmd, false);
-    if (sub_cs == NULL) {
-        return flags;
+    CommandSpecTable *sub_cmd_spec_table = RedisModule_DictGetC(sub_command_tables, cs->name, strlen(cs->name), NULL);
+    if (sub_cmd_spec_table == NULL) {
+        return flags; /* no table, return default set of flags for subcommand */
     }
 
-    return sub_cs->flags;
+    CommandSpec *sub_cs = getOrCreateCommandSpec(sub_cmd_spec_table, subcmd, false);
+    if (sub_cs == NULL) {
+        return flags;  /* command not specified in table, return default set of flags */
+    }
+
+    return sub_cs->flags;  /* command is in table, return its flags */
 }
 
 /* For a given RaftRedisCommandArray, return a flags value that represents
